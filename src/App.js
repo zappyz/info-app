b8ce8944760092b24c96a2c5478d4f39a5f4bd4d
zappy <helloworld.zapp@yahoo.com>
2022-10-21 20:23:29 +0200

    api test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState} from "react";
+import Axios from "axios";
+import "./App.css";
 
 function App() {
+
+  const [joke, setJoke] = useState("");
+
+  const getJoke = () => {
+    fetch("https://official-joke-api.appspot.com/random_joke")
+    .then((response) => response.json())
+    .then((data) => {
+        setJoke(data.setup + "..." + data.punchline);
+      });
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      Hello <button onClick={getJoke}>Get getJoke</button>
+      {joke}
     </div>
-  );
+  )
 }
 
 export default App;
